b3a9cb2d7d65ba547411e01980db4cd3b0a7c1c4
gustavokr87 <gustavokr@gmail.com>
2019-10-01 19:56:20 +0200

    addeded exo_05.rb finished exercise

---
+++ exo_05.rb
@@ -0,0 +1,39 @@
+
+#Cette ligne simplement puts le string
+puts "On va compter le nombre d'heures de travai à THP"
+
+#Cette ligne puts le string plus le resultat, donc "Travail : 550"
+puts "Travail : #{10 * 5 * 11}"
+
+#Cette ligne puts le string plus le resultat, donc ""En minutes ça fait 33000"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Cette ligne puts seulement le string "Et en secondes?"
+puts "Et en secondes?"
+
+#cette linge puts 1980000
+puts 10 * 5 * 11 * 60 * 60
+
+#cette ligne puts le string, il ne fait aucune calcule
+puts "Est-ce que c'est vrai que 3 +2 < 5 - 7?"
+
+#false
+puts 3 + 2 < 5 -7
+
+#5
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+
+#-2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#puts le string
+puts "C'est drôle ça, faisons-en plus :"
+
+#puts true
+puts "Est-ce que 5 est plus grande que -2 ? #{5 > -2}"
+
+#puts true
+puts "Est-ce que 5 est superieur ou égal à -2 ?#{5 >= -2}"
+
+#puts false
+puts "Est-ce que 5 est inférieur ou égal -2? #{5 <= -2}"
