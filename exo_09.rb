c11ea6207f479c973fa8013f547fae595de4bf61
gustavokr87 <gustavokr@gmail.com>
2019-10-02 00:48:13 +0200

    added ex9

---
+++ exo_09.rb
@@ -0,0 +1,11 @@
+puts "Hello, what's your name?"
+
+first_name = gets.chomp
+
+puts "Hi #{first_name}, thanks for the information."
+
+puts "Im afraid we need your full name:"
+
+last_names = gets.chomp
+
+puts "Perfect Mr. #{first_name} #{last_names}, you are now fully enrolled!"
